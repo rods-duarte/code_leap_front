cf0e6cc6a1d7de94e264ee93505b7bb9fffecfce
Rodrigo Duarte Maciel <rods.dduarte@gmail.com>
2023-05-08 02:36:36 -0300

    feat: add posts pagination (endless scrolling)

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setPosts } from '../../redux/slices/postsSlice';
 
@@ -9,12 +9,31 @@ import * as s from './style';
 
 export default function Posts() {
   const dispatch = useDispatch();
+  const [page, setPage] = useState(1);
   const { posts } = useSelector((store) => store.posts);
 
-  useEffect(() => {
-    api.getPosts().then((response) => {
-      dispatch(setPosts({ posts: response.data.results }));
+  const getPosts = (pageNum) => {
+    api.getPosts(pageNum * 10).then((response) => {
+      dispatch(setPosts({ posts: [...posts, ...response.data.results] }));
     });
+  };
+
+  const handleScroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop ===
+      document.documentElement.offsetHeight
+    ) {
+      setPage((prev) => prev + 1);
+    }
+  };
+
+  useEffect(() => {
+    getPosts(page);
+  }, [page]);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
   const content = posts.map((post) => <Post key={post.id} data={post} />);
