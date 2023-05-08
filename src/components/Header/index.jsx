bd7dafdd3eb72b11af5190e2ae5f700e48ae359e
Rodrigo Duarte Maciel <rods.dduarte@gmail.com>
2023-05-08 15:40:45 -0300

    feat: add logout button

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,31 @@
+import { BiLogOut } from 'react-icons/bi';
+import { useDispatch } from 'react-redux';
+import { resetUser, setUser } from '../../redux/slices/userSlice';
 import * as s from './style';
+import { setModalState } from '../../redux/slices/modalSlice';
 
 export default function Header() {
+  const dispatch = useDispatch();
+
+  const logout = () => {
+    dispatch(resetUser());
+
+    dispatch(
+      setModalState({
+        isOpen: true,
+        modalContent: 'signup',
+      })
+    );
+  };
+
   return (
     <s.Header>
-      <h1>CodeLeap Network</h1>
+      <s.Content>
+        <h1>CodeLeap Network</h1>
+        <s.Logout type="button" onClick={logout}>
+          <BiLogOut />
+        </s.Logout>
+      </s.Content>
     </s.Header>
   );
 }
--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -10,13 +10,39 @@ export const Header = styled.header`
   background-color: #7695ec;
 
   display: flex;
-  justify-content: flex-start;
   align-items: center;
-  padding-left: 30px;
+`;
+
+export const Content = styled.div`
+  width: 90%;
+  margin: 0 auto;
+  padding: 0 15px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 
   & > h1 {
     color: #fff;
     font-size: 22px;
     font-weight: 700;
+  }
+`;
+
+export const Logout = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: none;
+  background-color: #0000;
+  border: none;
+  color: #fff;
+  font-size: 22px;
+
+  cursor: pointer;
+
+  &:hover {
+    color: #ddd;
+    scale: 1.1;
+    transition: scale cubic-bezier(0.075, 0.82, 0.165, 1) 0.2s;
   }
 `;
